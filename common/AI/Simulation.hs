8f9d6baad03bc188d7fbadf6ecfada5a266580ba
Saveliy Skresanov <savask@yandex.ru>
2020-07-20 02:10:25 +0700

    Tweak simulation.

--- common/AI/Simulation.hs
+++ common/AI/Simulation.hs
@@ -55,20 +55,26 @@ findOrbitVel :: World -> Point -> Maybe Point
 findOrbitVel world p = if null variants then Nothing else return (head variants)
     where variants = filter (\v -> goodOrbit world (Object True p v)) [(x, y) | x <- [-10..10], y <- [-10..10]]
 
+neg (a, b) = (-a, -b)
+
 correctOrbit :: World -> Object -> Point
 correctOrbit world (Object _ pos@(x, y) vel@(vx, vy)) =
     if not (null vars) then head vars else defaultMove
     where vs = [(dx, dy) | dx <- [-1..1], dy <- [-1..1]]
           vars = filter (\(dx, dy) -> goodOrbit world (Object True pos (vx+dx, vy+dy))) vs
           rotate (a, b) = (b, -a)
-          neg (a, b) = (-a, -b)
           grav = normalize pos
           orbitvel = case findOrbitVel world pos of
                          Nothing -> (0, 0)
                          Just v -> v
           veldiff = orbitvel `vecp` vel
-          defaultMove | (abs x == abs y) || magn pos > 3*(planetR world) = (0, 0)
-                      | otherwise = normalize veldiff -- neg ((normalize $ (y, -x)) `vecp` grav)
+          minxy = min (abs vx) (abs vy)
+          maxxy = max (abs vx) (abs vy)
+          alpha = (fromIntegral maxxy) / (fromIntegral $ minxy^2)
+          defaultMove | alpha < 0.02 || (abs x == abs y) || magn pos > 3*(planetR world) = (0, 0)
+                      | abs x < abs y = (1, -(y `quot` (abs y)))
+                      | abs x > abs y = (-(x `quot` (abs x)), 1)
+                      | otherwise = neg $ normalize pos -- neg ((normalize $ (y, -x)) `vecp` grav)
 
 vecp (a, b) (c, d) = (a+c, b+d)
 
@@ -84,12 +90,19 @@ cleanWorld ws@(World b p f objs) = World b p f (filter (inBounds ws) objs)
 updateWorld :: World -> World
 updateWorld (World t p f objs) = cleanWorld (World t p f (map (moveObject . addGrav) objs))
 
+commandAlly :: World -> (Object -> Object) -> World
+commandAlly (World b p f objs) update = World b p f (map go objs)
+    where go o = if team o == b then update o else o
+
 applyAcc :: Object -> Point -> Object
 applyAcc (Object b p v) dv = Object b p (v `vecp` dv)
 
 produceInitialStats :: GameInfo -> IO Stats
-produceInitialStats gi = return $ Stats (total - 3 * 4 - 3 * 12 - 2 * 1) 3 3 1
+produceInitialStats gi = return $ Stats (total - mn * 4 - ch * 12 - tm * 2) mn ch tm
     where total = maxTotal $ maxStats gi
+          mn = 50
+          ch = 7
+          tm = 1
 
 coordToPoint :: Coord -> Point
 coordToPoint (Coord x y) = (x, y)
@@ -105,13 +118,18 @@ produceMoves gi (gs:_) = return $ concatMap shipAction myShips
           myShips = filter ally . map fst $ gameShips gs
           enemyShips = filter (not . ally) . map fst $ gameShips gs
           shipAction sh = 
-              let obj = Object (t == 1) (coordToPoint $ shipPos sh) (coordToPoint $ shipVel sh)
+              let obj = Object (t == 1) pos vel
+                  pos = coordToPoint $ shipPos sh
+                  grav = neg $ normalize pos
+                  vel = coordToPoint $ shipVel sh
+                  pos' = pos `vecp` vel `vecp` grav
                   (bx, by) = correctOrbit wrld obj
                   idd = shipId sh
                   enemy = head enemyShips
                   epos = coordToPoint $ shipPos enemy
                   evel = coordToPoint $ shipVel enemy
-                  epos' = epos `vecp` evel
-                  fire = if (not $ null enemyShips) && (t == 0)
-                         then [Laser idd (pointToCoord epos') 1] else []
+                  epos' = epos `vecp` evel `vecp` grav
+                  edist = magn (epos `vecp` (neg pos'))
+                  fire | (not $ null enemyShips) && (t == 0) = if edist <= 4 then [Detonate idd] else [Laser idd (pointToCoord epos') 7]
+                       | otherwise = []
               in [Boost idd (Coord bx by)] ++ fire
